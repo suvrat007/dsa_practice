e6fd0f8d37c9c23e56b95613d741b59a08848880
Suvrat <suvratmittal007@gmail.com>
2025-08-26 20:04:42 +0530

    1 leetcode question

--- LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring.java
@@ -1,12 +1,29 @@
-//package Grind75;
-//
-//public class LongestPalindromicSubstring {
-//    public String longestPalindrome(String s) {
-//
-//    }
-//    private boolean pal(int s , int e , String str){
-//        for (int i = 0; i < str.length(); i++) {
-//
-//        }
-//    }
-//}
+package Grind75;
+
+public class LongestPalindromicSubstring {
+    public String longestPalindrome(String s) {
+        if (s == null || s.length() < 2) return s;
+
+        int start =0 , maxLength=1;
+
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expand(s,i,i);
+            int len2 = expand(s,i,i+1);
+            int len = Math.max(len2,len1);
+            if (len>maxLength){
+                maxLength=len;
+                start = i - (len-1)/2;
+            }
+        }
+
+        return s.substring(start,start+maxLength);
+    }
+
+    private int expand (String s, int l, int r ){
+        while(l>=0 && r< s.length() && s.charAt(l)==s.charAt(r)){
+            l--;
+            r++;
+        }
+        return r-l-1;
+    }
+}
