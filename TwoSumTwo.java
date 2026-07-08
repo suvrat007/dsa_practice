156bda14d3b95553f1a5c6ea923587dff9217261
Suvrat <suvratmittal007@gmail.com>
2026-07-08 12:25:30 +0530

    2 leetcode question

---
+++ TwoSumTwo.java
@@ -0,0 +1,24 @@
+package Grind75;
+
+public class TwoSumTwo {
+    public int[] twoSum(int[] nums, int target) {
+
+        int i = 0;
+        int j = nums.length - 1;
+
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+
+            if (sum < target) {
+                i++;
+            } else if (sum > target) {
+                j--;
+            } else {
+                return new int[]{i + 1, j + 1};
+            }
+        }
+
+        return new int[]{-1, -1};
+
+    }
+}
